944c56973464ed77bf318e1e3cb8705522ede817
juanmi <juanmi1371989@hotmail.com>
2023-03-21 21:21:08 +0100

    extrayendo trabajo de clase

--- js/algoritmos2.js
+++ js/algoritmos2.js
@@ -1,5 +1,13 @@
 const randomNumbers = (max, min) => {
-    let random = Math.floor(Math.random() * (max - min + 1) + min);
-    return random;
-};   
+  let random = Math.floor(Math.random() * (max - min + 1) + min);
+  return random;
+};
 
+function e2p2(cad) {
+  var separarCadena = cad.split("");
+  var invertirArray = separarCadena.reverse();
+  var unirArrai = invertirArray.join("");
+  return unirArrai;
+}
+
+console.log(e2p2("no lemon, no melon"));
